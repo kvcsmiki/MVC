24443555f8b4d9de958793935cf7ab393b57730e
kvcsmiki <kovacs.miklos.szabolcs@gmail.com>
2022-04-25 08:48:36 +0200

    init

---
+++ src/main/java/BinaryCalculatorApplication.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BinaryCalculatorApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
