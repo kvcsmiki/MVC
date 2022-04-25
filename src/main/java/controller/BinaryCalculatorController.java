02c8705f69895ff34b83747519d9b89260761820
kvcsmiki <kovacs.miklos.szabolcs@gmail.com>
2022-04-25 09:02:23 +0200

    jaj

---
+++ src/main/java/controller/BinaryCalculatorController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class BinaryCalculatorController {
+    public void oszt(ActionEvent actionEvent) {
+    }
+
+    public void szoroz(ActionEvent actionEvent) {
+    }
+
+    public void kivon(ActionEvent actionEvent) {
+    }
+
+    public void osszead(ActionEvent actionEvent) {
+    }
+}
